ef105ee825cc1853f8b55be2675ed9cb409f5359
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-06-29 23:06:31 +1000

    cache yum rpms on host

--- lib/pdqtest/docker.rb
+++ lib/pdqtest/docker.rb
@@ -8,6 +8,9 @@ module PDQTest
     HIERA_YAML_CONTAINER = '/etc/puppetlabs/puppet/hiera.yaml'
     HIERA_YAML_HOST = '/spec/fixtures/hiera.yaml'
     HIERA_DIR  = '/spec/fixtures/hieradata'
+    HOST_CACHE_DIR = "#{Dir.home}/.pdqtest"
+    YUM_CACHE_HOST = "#{HOST_CACHE_DIR}/yum"
+    YUM_CACHE_CONTAINER = "/var/cache/yum"
 
     def self.wrap_cmd(cmd)
       ['bash',  '-c', "#{ENV} #{cmd}"]
@@ -33,6 +36,10 @@ module PDQTest
           []
         end
 
+      if ! Dir.exists?(YUM_CACHE_HOST)
+        FileUtils.mkdir_p(YUM_CACHE_HOST)
+      end
+
       # hiera.yaml *must* exist on the host or we will get errors from Docker
       if ! File.exists?(hiera_yaml_host)
         File.write(hiera_yaml_host, '# hiera configuration for testing')
@@ -41,10 +48,11 @@ module PDQTest
         'Image'   => IMAGE_NAME,
         'Volumes' => {
           test_dir              => {pwd               => 'rw'},
-          HIERA_YAML_CONTAINER  => {hiera_yaml_host   => 'ro'},
-          HIERA_DIR             => {hiera_dir         => 'ro'},
+          HIERA_YAML_CONTAINER  => {hiera_yaml_host   => 'rw'},
+          HIERA_DIR             => {hiera_dir         => 'rw'},
           '/cut'                => {pwd               => 'rw'}, # DEPRECATED -FOR REMOVAL
           '/sys/fs/cgroup'      => {'/sys/fs/cgroup'  => 'ro'},
+          YUM_CACHE_CONTAINER   => {YUM_CACHE_HOST    => 'rw'},
         },
         'HostConfig' => {
           "SecurityOpt" => security_opt,
@@ -54,6 +62,7 @@ module PDQTest
             "#{pwd}:#{test_dir}:rw",
             "#{hiera_yaml_host}:#{HIERA_YAML_CONTAINER}:rw",
             "#{hiera_dir}:#{HIERA_DIR}:rw",
+            "#{YUM_CACHE_HOST}:#{YUM_CACHE_CONTAINER}:rw",
           ],
         },
       )
